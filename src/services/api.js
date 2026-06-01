23e4edf59d0ff4679eeb45b862f580dd0e4d8839
Rafly Ramadhan <Raflyraflyramadhan7477@gmail.com>
2026-06-02 01:39:20 +0700

    Update API endpoint to production URL on Railway

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8000';
+export const API_URL = 'https://hanafi-be-production.up.railway.app';
 
 /**
  * Predefined clinical presets to test prediction capabilities.
